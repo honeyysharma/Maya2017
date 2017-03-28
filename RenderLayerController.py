4294b7b02c7ff0d6eb05471cb49455d70d08b1e8
honeyysharma <honey.y.sharma@gmail.com>
2017-03-29 00:43:26 +0530

    No need to create all light collection in Maya2017

---
+++ RenderLayerController.py
@@ -0,0 +1,46 @@
+import sys
+from RenderLayers import EnvirLayer, CharLayer
+
+class RenderLayerController(object):
+    def __init__(self):
+        pass
+        
+    def createEnvirLayer(self, layerName):
+        layer = EnvirLayer(layerName)
+        #layer.createCollectionForAllLights()
+        layer.turnOffCharLights()
+        layer.turnOffAllChar(False)
+        layer.createAllEnvirCollection()
+        layer.switchToLayer()
+    
+    def createCharLayer(self, layerName):
+        layer = CharLayer(layerName)
+        #layer.createCollectionForAllLights()
+        layer.turnOffEnvirLights()
+        layer.turnOffAllEnvir(True)
+        layer.createAllCharCollection()
+        layer.switchToLayer()
+        
+    def createCustomEnvirLayer(self, layerName, isCutoutChecked):
+        layer = EnvirLayer(layerName)
+        #layer.createCollectionForAllLights()
+        layer.turnOffCharLights()
+        
+        #toggle only Envir cutout
+        layer.turnOffAllEnvir(isCutoutChecked)
+        
+        layer.createCustomEnvirCollection(isCutoutChecked)
+        layer.turnOffAllChar(False)
+        layer.switchToLayer()
+        
+    def createCustomCharLayer(self, layerName, isCutoutChecked):
+        layer = CharLayer(layerName)
+        #layer.createCollectionForAllLights()
+        layer.turnOffEnvirLights()
+        layer.turnOffAllEnvir(True)
+        
+        #toggle only Char cutout
+        layer.turnOffAllChar(isCutoutChecked)
+        
+        layer.createCustomCharCollection(isCutoutChecked)
+        layer.switchToLayer()
